7b09fcaa6ec3c4ae525f0ca0b816ce6d6f1ef4e7
刘乐 <liule_17173@cyou-inc.com>
2019-05-24 20:50:48 +0800

    提交全排列算法，暂未完成

---
+++ src/Permutations.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 数组全排列
+ * Input: [1,2,3]
+ * Output:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
+ */
+public class Permutations {
+    private HashMap<Integer, List<Integer>> hashMap = new HashMap<>();
+
+    public static void main(String[] args) {
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+
+}
