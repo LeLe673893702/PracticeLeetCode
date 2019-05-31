2a52ab8e41f20839c9dd563e299c61b0645bfd60
LeLe673893702 <673893702@qq.com>
2019-05-31 23:10:10 +0800

    完善top k算法

--- src/KthLargestElementInArray.java
+++ src/KthLargestElementInArray.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 215.数组中的第K个最大元素
@@ -10,7 +13,7 @@ public class KthLargestElementInArray {
     public static void main(String[] args) {
         KthLargestElementInArray kthLargestElementInArray = new KthLargestElementInArray();
         int[] nums = {3,2,1,5,6,4};
-        System.out.println(kthLargestElementInArray.findKthLargest(nums, 2));
+        System.out.println(kthLargestElementInArray.findKthLargest1(nums, 2));
     }
 
     public int findKthLargest(int[] nums, int k) {
@@ -27,7 +30,7 @@ public class KthLargestElementInArray {
         n--;
         order--;
         while (order > 0) {
-             deleteHeap(datas, n);
+            deleteMax(datas, n, 1);
 
             // 第一个为最大值，放到最后面
             swap(n-1, 1, datas);
@@ -43,43 +46,30 @@ public class KthLargestElementInArray {
     private void buildBigHeap(int[] datas, int n) {
         // 从第一个非叶子节点进行比较
         for (int i = n / 2; i > 0; i--) {
-            int maxPos = i;
-
-            // 和左节点比较
-            if (2*i < n && datas[i] < datas[2*i]) {
-                maxPos = 2 * i;
-            }
-
-            // 和右节点比较
-            if (2*i + 1 < n && datas[maxPos] < datas[2*i+1]) {
-                maxPos = 2 * i + 1;
-            }
-
-            if (maxPos != i) {
-                swap(maxPos, i , datas);
-            }
+            deleteMax(datas, n, i);
         }
     }
 
-    private void deleteHeap(int[] datas, int n) {
-        for (int i = 1; i <= n/2; i++) {
+    private void deleteMax(int[] datas, int n, int i) {
+        while (true) {
             int maxPos = i;
 
             // 和左节点比较
-            if (2*i < n && datas[i] < datas[2*i]) {
+            if (2 * i < n && datas[i] < datas[2 * i]) {
                 maxPos = 2 * i;
             }
 
             // 和右节点比较
-            if (2*i + 1 < n && datas[maxPos] < datas[2*i+1]) {
+            if (2 * i + 1 < n && datas[maxPos] < datas[2 * i + 1]) {
                 maxPos = 2 * i + 1;
             }
 
             if (maxPos != i) {
-                swap(maxPos, i , datas);
+                swap(maxPos, i, datas);
             } else {
                 break;
             }
+            i = maxPos;
         }
     }
 
@@ -87,5 +77,17 @@ public class KthLargestElementInArray {
         int tmp = datas[i];
         datas[i] = datas[j];
         datas[j] = tmp;
+    }
+
+    public int findKthLargest1(int[] nums, int k) {
+        final PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int val : nums) {
+            pq.offer(val);
+
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
     }
 }
