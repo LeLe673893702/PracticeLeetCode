87a73d5cde8a0d78f0c3bc94825ea58528aef2e4
刘乐 <liule_17173@cyou-inc.com>
2019-06-03 22:01:54 +0800

    完善获取第一个没有出现的正整数算法

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 求最小正整数
  */
 public class FirstMissingPositive {
     public static void main(String[] args) {
-        int[] nums = {3,4,-1,1};
-        System.out.println(firstMissingPositive(nums));
+        int[] nums = {-1,4,2,1,9,10};
+        System.out.println(firstMissingPositive2(nums));
     }
-    public static int firstMissingPositive(int[] nums) {
+    public static int firstMissingPositive1(int[] nums) {
         Arrays.sort(nums);
         int min = 1;
         for(int i = 0; i < nums.length; i++) {
@@ -24,4 +26,31 @@ public class FirstMissingPositive {
         }
         return min;
     }
+
+    public static int firstMissingPositive2(int[] nums) {
+        for (int i = 0; i < nums.length;) {
+            if (nums[i] > 0 && nums[i] < nums.length) {
+                // 交换两个数字不能相等
+                if (nums[i] != nums[nums[i] - 1]) {
+                    swap(i, nums[i] - 1, nums);
+                    continue;
+                }
+            }
+            i++;
+        }
+
+        for (int i = 0; i <nums.length; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return nums.length+1;
+    }
+
+    public static void swap(int i, int j, int[] nums) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
 }
