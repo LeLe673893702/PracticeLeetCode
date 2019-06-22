7dd7a4d0603563f36eb8461703edee5e909fd263
LeLe673893702 <673893702@qq.com>
2019-06-22 19:42:40 +0800

    增加字符串算法

---
+++ src/string/ReverseString.java
@@ -0,0 +1,18 @@
+package string;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                char tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+            }
+            i++;
+            j--;
+        }
+    }
+
+}
