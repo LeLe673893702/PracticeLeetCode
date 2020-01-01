49fc07971f5aac5eee65a0756f11aaca2255dd5a
LeLe673893702 <673893702@qq.com>
2020-01-01 23:53:56 +0800

    删除排序数组中的重复项

---
+++ src/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,45 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * 26. 删除排序数组中的重复项
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+        removeDuplicates(nums);
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+    public static int removeDuplicates(int[] nums) {
+        int sum = 0;
+        int index = -1;
+        boolean first = true;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i+1]) {
+                nums[i] = Integer.MAX_VALUE;
+                sum++;
+            }
+            if (first) {
+                index = i;
+                first = false;
+            }
+        }
+
+        if (index == -1) {
+            return nums.length - sum;
+        }
+
+        for (int i = index; i < nums.length -1; i++) {
+            if (nums[i+1] != Integer.MAX_VALUE) {
+                int swap = nums[i+1];
+                nums[i+1] = nums[index];
+                nums[index] = swap;
+                index++;
+            }
+
+        }
+        return nums.length - sum;
+    }
+}
